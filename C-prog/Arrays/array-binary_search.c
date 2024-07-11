a9f03a6763e62d3436c7f96c0e53baa303abac3d
franklinegit <oddfrankline@gmail.com>
2024-07-11 19:49:06 +0000

    Binary search algorithm

--- C-prog/Arrays/array-binary_search.c
+++ C-prog/Arrays/array-binary_search.c
@@ -1,16 +1,35 @@
-#include <stdio.h>
+#include "main.h"
 
-void SortArray(int arr[]);
-int TargetIndex(int arr[]);
-int GetTarget();
-void DisplayArrayElements();
-
-
-int main(void)
+/**
+ * @brief Function finds a target by binary search
+ * 
+ * @param arr Array of integers
+ * @param size Size of the array
+ * @param target Target to be found in the array
+ * @return int 
+ */
+int BinarySearch(int arr[], int size, int target)
 {
-    int arr[] = {3, 75, 43, 18, 51, 9, 14, 57, 83, 26};
+    int start = 0, end = size - 1;
 
-    SortArray(arr);
+    printf("Performing Binary Search...\n\n");
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
 
-    return (0);
+        if (arr[mid] == target)
+        {
+            printf("Target found in the array at index %d.\n\n", mid);
+            return (mid);
+        }
+        else if (target > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid -1;
+        }
+    }
+    return (-1);
 }
