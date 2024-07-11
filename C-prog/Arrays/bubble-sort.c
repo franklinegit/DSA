bca6c9ce355c4d3e5f05da93528f7bce02f10b43
franklinegit <oddfrankline@gmail.com>
2024-07-11 12:56:45 +0000

    Bubble sorting

---
+++ C-prog/Arrays/bubble-sort.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * @brief Function that sorts an array through bubble sorting
+ * 
+ * @param arr Array to be sorted
+ * @param size Size of array to be sorted
+ */
+void BubbleSort(int arr[], int size)
+{
+    /* Bubble Sort repeatedly steps through the list, 
+    compares adjacent elements, and swaps them if they are in the wrong order. 
+    It continues this process until the list is sorted. */
+    int i =0, j = 0, temp = 0;
+    
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                /* Swap elements */
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
