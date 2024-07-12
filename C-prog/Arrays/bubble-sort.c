9fd16c76461fef466f948e7e426906695f4aa47f
franklinegit <oddfrankline@gmail.com>
2024-07-12 16:27:08 +0000

    Bubble  sorting algorithm

--- C-prog/Arrays/bubble-sort.c
+++ C-prog/Arrays/bubble-sort.c
@@ -9,9 +9,9 @@
  */
 void BubbleSort(int arr[], int size)
 {
-    /* Bubble Sort repeatedly steps through the list, 
-    compares adjacent elements, and swaps them if they are in the wrong order. 
-    It continues this process until the list is sorted. */
+    /* Bubble Sort repeatedly steps through the list, */
+    /* compares adjacent elements, and swaps them if they are in the wrong order. */
+    /* It continues this process until the list is sorted. */
     int i =0, j = 0, temp = 0;
     
     printf("Selection sorting the array...\n");
