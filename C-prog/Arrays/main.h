068a8e4e05582afd507307dbd6f1810dd0fbce55
franklinegit <oddfrankline@gmail.com>
2024-07-12 16:15:50 +0000

    main.h

---
+++ C-prog/Arrays/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void BubbleSort(int arr[], int size);
+int BinarySearch(int arr[], int size, int target);
+void SelectionSort(int arr[], int size);
+void SortArray(int arr[]);
+int TargetIndex(int arr[]);
+int GetTarget();
+void DisplayArrayElements();
+
+#endif
